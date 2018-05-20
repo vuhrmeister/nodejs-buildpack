b7c6b1a816c397ab1c3bad70e98fbec9b990ebe0
Valentin Uhrmeister <kontakt@ufisch.net>
2018-05-20 14:15:07 +0200

    foo

--- bin/meteor.sh
+++ bin/meteor.sh
@@ -134,6 +134,7 @@ install_meteor_dist() {
   tarball_url="https://github.com/meteor/meteor/archive/release/METEOR@1.7-rc.11.tar.gz"
 
   header "Downloading Meteor distribution"
+  header tarball_url
   curl --silent --fail "${tarball_url}" | tar -xzf - -C "${METEOR_HOME}" -o
 
   header "Meteor ${release} has been installed."
